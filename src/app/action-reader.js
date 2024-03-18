3e1c3a54448f4b0bf589cae4d399834bfca8f9f6
LeonAdeoye <leon.adeoye@gmail.com>
2024-03-18 22:53:07 +0800

    added request reply to web worker

--- src/app/action-reader.js
+++ src/app/action-reader.js
@@ -1,24 +1,40 @@
 const {Client, Command} = require('amps');
 
+const clientName = "action-dispatch-processor";
+const url = "ws://localhost:9008/amps/json";
+const client = new Client(clientName);
+
+// eslint-disable-next-line no-restricted-globals
+self.addEventListener('message', async (event) =>
+{
+    try
+    {
+        await client.publish('action-dispatch-response', JSON.stringify(event.data));
+    }
+    catch (e)
+    {
+        console.log(e);
+    }
+});
+
 const main = async () =>
 {
     try
     {
-        // echo '[{"type": "cake/ordered"},{"type": "cake/restocked", "payload": 10}]' | ./spark publish -topic action-dispatch-reader -server localhost:9007 -type json -proto amps
-        // ./spark subscribe -server localhost:9007 -topic action-dispatch-reader -type json -proto amps
-        const clientName = "action-dispatch-reader";
-        const topicName = "action-dispatch-reader";
-        const url = "ws://localhost:9008/amps/json";
-        const client = new Client(clientName);
+        // echo '[{"type": "cake/ordered"},{"type": "cake/restocked", "payload": 10}]' | ./spark publish -topic action-dispatch-request -server localhost:9007 -type json -proto amps
+        // ./spark subscribe -server localhost:9007 -topic action-dispatch-response -type json -proto amps
         await client.connect(url);
-        const cmd = new Command("subscribe").topic(topicName);
-        await client.execute(cmd, (message) => postMessage({messageType: "actionDispatched", actions: message.data}));
-        console.log("Action dispatch reader web worker Connected to AMPS using URL: ", url);
+        const cmd = new Command("subscribe").topic("action-dispatch-request");
+        await client.execute(cmd, (message) =>
+        {
+            postMessage({messageType: "actionDispatched", actions: message.data});
+        });
     }
     catch (e)
     {
-        console.error(e);
+        console.log(e);
     }
-}
+};
+
 
 main().then(() => console.log("AMPS action dispatch subscription completed."));
--- src/app/store.ts
+++ src/app/store.ts
@@ -6,6 +6,13 @@ import userReducer from '../features/user/userSlice';
 // @ts-ignore
 //import reduxLogger from 'redux-logger';
 //import { Middleware } from '@reduxjs/toolkit';
+// const customLoggerMiddleware: Middleware = (store) => (next) => (action) =>
+// {
+//     console.log('dispatching', action);
+//     const result = next(action);
+//     console.log('next state', store.getState());
+//     return result
+// }
 
 const worker = new Worker(new URL('./action-reader.js', import.meta.url));
 
@@ -15,27 +22,23 @@ type actionToDispatchType =
     payload?: number
 }
 
-// Custom middleware created using the Middleware type from Redux Toolkit
-// const customLoggerMiddleware: Middleware = (store) => (next) => (action) =>
-// {
-//     console.log('dispatching', action);
-//     const result = next(action);
-//     console.log('next state', store.getState());
-//     return result
-// }
-
 const store = configureStore({
     reducer: {
         cake: cakeReducer,
         icecream: icecreamReducer,
         user: userReducer
     },
-    // middleware: (getDefaultMiddleware) => getDefaultMiddleware()
-    //     .concat(reduxLogger).concat(customLoggerMiddleware)
+    // middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(reduxLogger).concat(customLoggerMiddleware)
 });
 
-type AnyObject = Record<string, any>;
+const getAllActionStates = () =>
+{
+    const actionStates: any[] = [];
+    Object.values(store.getState()).forEach((slice) => actionStates.push(slice));
+    return actionStates;
+}
 
+type AnyObject = Record<string, any>;
 function findChangedProperties<T extends AnyObject, U extends AnyObject>(a: T, b: U): Partial<U>
 {
     const changedProperties: Map<string, any> = new Map();
@@ -67,8 +70,6 @@ function findChangedProperties<T extends AnyObject, U extends AnyObject>(a: T, b
     return result;
 }
 
-// echo '[{"type": "cake/ordered"},{"type": "cake/restocked", "payload": 10}]' | ./spark publish -topic action-dispatch-reader -server localhost:9007 -type json -proto amps
-
 worker.addEventListener('message', (event) =>
 {
     const { data } = event;
@@ -90,8 +91,7 @@ worker.addEventListener('message', (event) =>
                 store.dispatch(actionToDispatch);
                 let newState: any = getAllActionStates();
                 let delta = findChangedProperties(previousState, newState);
-
-                console.log(JSON.stringify({ action: actionToDispatch, delta: delta, previousState: previousState, newState: newState}));
+                worker.postMessage({timestamp: Date.now(), action: actionToDispatch, delta: delta, previousState: previousState, newState: newState});
             }
             else
                 console.log("Action dispatch message received from worker does not contain a type property.");
@@ -100,17 +100,6 @@ worker.addEventListener('message', (event) =>
     else
         console.log("Action dispatch message received from worker does not contain an array of actions.");
 });
-
-
-
-const getAllActionStates = () =>
-{
-    const actionStates: any[] = [];
-
-    Object.values(store.getState()).forEach((slice) => actionStates.push(slice));
-
-    return actionStates;
-}
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
