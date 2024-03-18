7703f6fe078af15fe2d22283650659d2ff6a5421
LeonAdeoye <leon.adeoye@gmail.com>
2024-03-18 21:42:44 +0800

    log previous state, action, next state

--- src/app/action-reader.js
+++ src/app/action-reader.js
@@ -4,13 +4,15 @@ const main = async () =>
 {
     try
     {
+        // echo '[{"type": "cake/ordered"},{"type": "cake/restocked", "payload": 10}]' | ./spark publish -topic action-dispatch-reader -server localhost:9007 -type json -proto amps
+        // ./spark subscribe -server localhost:9007 -topic action-dispatch-reader -type json -proto amps
         const clientName = "action-dispatch-reader";
         const topicName = "action-dispatch-reader";
         const url = "ws://localhost:9008/amps/json";
         const client = new Client(clientName);
         await client.connect(url);
         const cmd = new Command("subscribe").topic(topicName);
-        await client.execute(cmd, (message) => postMessage({messageType: "actionDispatched", action: message.data}));
+        await client.execute(cmd, (message) => postMessage({messageType: "actionDispatched", actions: message.data}));
         console.log("Action dispatch reader web worker Connected to AMPS using URL: ", url);
     }
     catch (e)
--- src/app/store.ts
+++ src/app/store.ts
@@ -1,14 +1,16 @@
-import { configureStore } from '@reduxjs/toolkit';
-//import { Middleware } from '@reduxjs/toolkit';
+import {configureStore} from '@reduxjs/toolkit';
 import cakeReducer from '../features/cake/cakeSlice';
 import icecreamReducer from '../features/icecream/icecreamSlice';
 import userReducer from '../features/user/userSlice';
 
 // @ts-ignore
-import reduxLogger from 'redux-logger';
+//import reduxLogger from 'redux-logger';
+//import { Middleware } from '@reduxjs/toolkit';
+
 const worker = new Worker(new URL('./action-reader.js', import.meta.url));
 
-type actionToDispatchType = {
+type actionToDispatchType =
+{
     type: string,
     payload?: number
 }
@@ -28,26 +30,87 @@ const store = configureStore({
         icecream: icecreamReducer,
         user: userReducer
     },
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware()
-        .concat(reduxLogger)//.concat(customLoggerMiddleware)
+    // middleware: (getDefaultMiddleware) => getDefaultMiddleware()
+    //     .concat(reduxLogger).concat(customLoggerMiddleware)
 });
+
+type AnyObject = Record<string, any>;
+
+function findChangedProperties<T extends AnyObject, U extends AnyObject>(a: T, b: U): Partial<U>
+{
+    const changedProperties: Map<string, any> = new Map();
+
+    // Helper function to recursively compare properties
+    const compareProperties = (objA: AnyObject, objB: AnyObject, path: string = ''): void => {
+        for (const key in objA) {
+            if (typeof objA[key] === 'object' && typeof objB[key] === 'object') {
+                compareProperties(objA[key], objB[key], `${path}${key}.`);
+            } else if (objA.hasOwnProperty(key) && objB.hasOwnProperty(key) && objA[key] !== objB[key]) {
+                changedProperties.set(`${path}${key}`, objB[key]);
+            }
+        }
+    };
+
+    // Start comparison
+    compareProperties(a, b);
+
+    // Convert Map to object
+    const result: Partial<U> = {};
+    changedProperties.forEach((value, key) => {
+        const propName = key.split('.').pop(); // Get the last part of the key
+        if (propName) {
+            // @ts-ignore
+            result[propName] = value;
+        }
+    });
+
+    return result;
+}
+
+// echo '[{"type": "cake/ordered"},{"type": "cake/restocked", "payload": 10}]' | ./spark publish -topic action-dispatch-reader -server localhost:9007 -type json -proto amps
 
 worker.addEventListener('message', (event) =>
 {
-    console.log('Store received dispatch action message:', event.data);
-    const {data} = event;
-    if(data.action)
-    {
-        let actionToDispatch: actionToDispatchType =
-        {
-            type: data.action.type
-        }
-        if(data.action.payload)
-            actionToDispatch['payload'] = data.action.payload;
+    const { data } = event;
 
-        store.dispatch(actionToDispatch);
+    if (Array.isArray(data.actions)) {
+        data.actions.forEach((action: actionToDispatchType) =>
+        {
+            if (action.type)
+            {
+                const actionToDispatch: actionToDispatchType =
+                {
+                    type: action.type,
+                };
+
+                if (action.payload)
+                    actionToDispatch.payload = action.payload;
+
+                let previousState: any = getAllActionStates();
+                store.dispatch(actionToDispatch);
+                let newState: any = getAllActionStates();
+                let delta = findChangedProperties(previousState, newState);
+
+                console.log(JSON.stringify({ action: actionToDispatch, delta: delta, previousState: previousState, newState: newState}));
+            }
+            else
+                console.log("Action dispatch message received from worker does not contain a type property.");
+        });
     }
+    else
+        console.log("Action dispatch message received from worker does not contain an array of actions.");
 });
+
+
+
+const getAllActionStates = () =>
+{
+    const actionStates: any[] = [];
+
+    Object.values(store.getState()).forEach((slice) => actionStates.push(slice));
+
+    return actionStates;
+}
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
